84bee35c648bacb4c5d013d7e937b7c1b19d8dab
mateuszkowalczykk <x625134x@gmail.com>
2020-11-24 19:20:49 +0100

    add sprint's descriptions by js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
+var sprintDescriptions = ["<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam, culpa delectus, dicta inventore itaque laborum magnam minima neque quasi reiciendis repudiandae? Illo ipsam, voluptatem!</p>",
+    "<p>Accusantium alias animi aut, cum debitis dicta ipsa laudantium maiores minima nemo obcaecati rem repudiandae ullam? A at dolorem ex id illo ipsum pariatur similique sint unde voluptatibus. Aliquam animi assumenda beatae culpa cumque ducimus et illo incidunt ipsum laborum libero magni, modi mollitia nemo nobis nulla obcaecati optio perspiciatis quaerat quasi, quisquam reiciendis rerum, sapiente sequi totam unde ut vel?</p>",
+    "<p>A at dolorem ex id illo ipsum pariatur similique sint unde voluptatibus. Aliquam animi assumenda beatae culpa cumque ducimus et illo incidunt ipsum laborum libero magni, modi mollitia nemo nobis nulla obcaecati optio perspiciatis quaerat quasi, quisquam reiciendis rerum, sapiente sequi totam unde ut vel? Animi dolorem eaque expedita id quas quisquam quod sunt vitae! Aliquid deleniti eum reprehenderit!</p>",
+    "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquam, culpa delectus, dicta inventore itaque laborum magnam minima neque quasi reiciendis repudiandae? Illo ipsam, voluptatem! Accusantium alias animi aut, cum debitis dicta ipsa laudantium maiores minima nemo obcaecati rem repudiandae ullam? </p>",
+    "<p>Accusamus aliquam, culpa delectus, dicta inventore itaque laborum magnam minima neque quasi reiciendis repudiandae? Illo ipsam, voluptatem! Accusantium alias animi aut, cum debitis dicta ipsa laudantium maiores minima nemo obcaecati rem repudiandae ullam? A at dolorem ex id illo ipsum pariatur similique sint unde voluptatibus. Aliquam animi assumenda beatae culpa cumque ducimus et illo incidunt ipsum laborum libero magni, modi mollitia nemo nobis nulla </p>"];
+var buttons = document.getElementsByClassName("button");
+var description = document.getElementById("description");
 
-function changeLightBlueRectangle(){
-    var rectangles = document.getElementsByClassName("LightBlueRectangle");
-    var color = rectangles[0].style.fill  == "yellow"?"#76BDF9":"yellow";
-    for (var i=0; i<rectangles.length; i++) {
-      rectangles[i].style.fill = color;
+changeSprint(1);
+
+function changeSprint(sprintNumber){
+    for (var i=0; i<buttons.length; i++) {
+          buttons[i].style.backgroundColor = "#D092CB";
     }
+    buttons[sprintNumber-1].style.backgroundColor = "#8C037F";
+    description.innerHTML = "<h3>Sprint "+sprintNumber+"</h3>"+sprintDescriptions[sprintNumber-1];
 }
 
-function changeDarkBlueRectangle(){
-    var rectangles = document.getElementsByClassName("DarkBlueRectangle");
-    console.log(rectangles[0].style.fill);
-    var color = rectangles[0].style.fill  == "green"?"#000360":"green";
-    for (var i=0; i<rectangles.length; i++) {
-      rectangles[i].style.fill = color;
-    }
-}
+
+
+
